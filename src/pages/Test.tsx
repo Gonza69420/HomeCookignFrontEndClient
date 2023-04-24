419fb03e70ac1940d0047904855f9a1f4f78f3ca
Gonza69420 <gonzalo.mato@ing.austral.edu.ar>
2023-04-23 22:21:07 -0300

    chat checkpoint

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -4,42 +4,17 @@ import {CreateSolicitude} from "../components/CreateSolicitude/CreateSolicitude.
 import {DateSolicitude} from "../objects/Date";
 import {MenuChef} from "../objects/Menu";
 import {Button} from "@mui/material";
-
+import { Chat } from './chat/Chat';
+import {RecoilRoot} from "recoil";
 export const Test = () => {
     const [open, setOpen] = useState<boolean>(false);
 
-
-    const [allowedDates, setAllowedDates] = useState<DateSolicitude[]>([
-        {
-                    date: new Date(2023,0,26),
-                    hour: ["10:00-20:30"]
-        },
-        {
-                    date: new Date(2023,1,2),
-                    hour: ["10:00-20:30"]
-        },
-        {
-                    date: new Date(2023,1,5),
-                    hour: ["10:00-12:30" , "13:00-20:30"]
-        }
-    ]);
-
-    const [menus, setMenus] = useState<MenuChef[]>([
-        {
-            name: "Menu 1",
-            price: 1000,
-        },
-        {
-            name: "Menu 2",
-            price: 2000,
-        }
-        ]);
-
-
     return(
         <>
-            <Button onClick={()=> setOpen(true)}>Pene</Button>
-            <CreateSolicitude setClose={setOpen} open={open} chefName={"Gonzalo Prongra"} allowedDates={allowedDates} menus={menus} imageURL={"https://firebasestorage.googleapis.com/v0/b/homecooking-346302.appspot.com/o/images%2Fchef%2Fchefsito%40gmail.com?alt=media&token=a540b2e2-940d-4c4e-bf1a-0ebcf3a108cc"}/>
+            <h1>afdsadsf</h1>
+            <RecoilRoot>
+                <Chat></Chat>
+            </RecoilRoot>
         </>
     )
 }
